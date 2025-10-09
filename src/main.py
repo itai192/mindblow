0a4243b6fbf3d3bff4ded82ff51cb6103e9a92b2
shemishtamesh <shemishtamail@gmail.com>
2025-10-09 20:51:59 +0300

    make documentation correct

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
-import mindblow
 import argparse
 import os
-import locale
+import re
+
+import mindblow
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "file", help="name of the file being compiled, has end with .🤯", type=str
+        "file",
+        help="name of the file being compiled, has to end with `.` followed by one or more `🤯`",
+        type=str,
     )
     parser.add_argument(
         "-o",
@@ -21,10 +24,12 @@ def main() -> None:
     if out_file_path is None:
         out_file_path = in_file_path + ".o"
     validate_input(in_file_path, out_file_path)
-    in_file_content = ""
+
     with open(in_file_path, encoding="utf_8") as in_file_d:
         in_file_content = in_file_d.read()
-    output = mindblow.compile(in_file_content)
+    number_of_leading_zeros = in_file_path.rsplit(".", 1)[1].count("🤯") - 1
+    output = mindblow.compile(in_file_content, number_of_leading_zeros)
+
     with open(out_file_path, "wb") as out_fd:
         out_fd.write(output)
     print("done")
@@ -33,7 +38,9 @@ def main() -> None:
 def validate_input(in_file: str, out_file: str) -> None:
     assert isinstance(out_file, str), "there has to be an output path"
     assert isinstance(in_file, str), "there has to be an input path"
-    assert in_file.split(".")[-1] == "🤯", "the input path has to end with .🤯"
+    assert re.match(
+        r".*\.🤯+$", in_file
+    ), "the input path has to and with one or more `🤯` after a `.`"
     assert os.path.isfile(in_file), f"the file {in_file} doesn't exist🤯🤯🤯🤯🤯🤯"
 
 
